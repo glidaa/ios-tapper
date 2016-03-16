973402c618e608b4ff2df100ebc694c0fba91225
Michael Castleman <michael@smartformguide.com>
2016-03-17 10:12:48 +1100

    added variables

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
 
 }
 
