5fe6d09215d86e838ee4f7f33b5918be5a230158
Michael Castleman <michael@smartformguide.com>
2016-03-17 14:26:37 +1100

    update coin and play working

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -18,15 +18,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var playBtn: UIButton!
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
-    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+            currentTaps++
+            updateTapsLbl()
+    }
     @IBAction func onPlayBtnPressed(sender: UIButton!) {
-        logoImg.hidden = true
-        playBtn.hidden = true
-        howManyTapsTxt.hidden = true
-        
-        tapBtn.hidden = false
-        tapsLbl.hidden = false
+                if howManyTapsTxt != nil && howManyTapsTxt.text != "" {
+                    logoImg.hidden = true
+                    playBtn.hidden = true
+                    howManyTapsTxt.hidden = true
+                    
+                    tapBtn.hidden = false
+                    tapsLbl.hidden = false
+                    maxTaps = Int(howManyTapsTxt.text!)!
+                    currentTaps = 0
+                    updateTapsLbl()
+
+        }
     }
     
+    func updateTapsLbl() {
+            tapsLbl.text = "\(currentTaps) Taps"
+    }
 }
 
